24ec0c1d9859c95c2c804a7c503fdd1236cc9dcd
doganparlak <dgnprlk@gmail.com>
2024-09-05 14:00:55 +0300

    a scheduler which deletes the TrainedModels table every 2 days

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
-from tables import db
+from tables import db, TrainedModels
 from routes import init_routes
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
 
 def create_app():
     app = Flask(__name__)
@@ -13,9 +15,28 @@ def create_app():
     
     return app
 
+# Wrapper function to call the TrainedModels.delete_all_models method within the app context
+def delete_all_models_wrapper():
+    with app.app_context():  # Ensure the function is running within the app context
+        TrainedModels.delete_all_models()
+
+# Function to create and start the scheduler
+def create_scheduler(app):
+    scheduler = BackgroundScheduler()
+    # Pass the wrapper function to the scheduler
+    scheduler.add_job(func=delete_all_models_wrapper, trigger="interval", days=2)
+    scheduler.start()
+
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
+
+# Initialize Flask app and Scheduler
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        print(TrainedModels.query.all())
+        create_scheduler(app)
+
     app.run(debug=True)
